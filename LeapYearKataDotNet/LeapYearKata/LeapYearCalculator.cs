60bdf061ef5c839cc95e96f45a6eb837ea6db78f
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-13 13:05:20 +0100

    :fire: remove unnecessary code

--- LeapYearKataDotNet/LeapYearKata/LeapYearCalculator.cs
+++ LeapYearKataDotNet/LeapYearKata/LeapYearCalculator.cs
@@ -2,10 +2,6 @@
 {
     public class LeapYearCalculator
     {
-        public LeapYearCalculator()
-        {
-        }
-
         public bool Isleap(int yearNumber)
         {
             var year = new Year(yearNumber);
