087500c18a3b79466ce3624fd2f119d2d3ea6063
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-13 13:18:04 +0100

    :test_tube: add red test: be_leap_if_is_divisible_by_4_and_not_by_100

--- LeapYearKataDotNet/LeapYearKata.Tests/LeapYearCalculatorFeature.cs
+++ LeapYearKataDotNet/LeapYearKata.Tests/LeapYearCalculatorFeature.cs
@@ -7,6 +7,18 @@ namespace LeapYearKata.Tests
     {
         private LeapYearCalculator calculator;
 
+        [TestCase(2008)]
+        [TestCase(2012)]
+        [TestCase(2016)]
+        public void be_leap_if_is_divisible_by_4_and_not_by_100(int yearNumber)
+        {
+            var calculator = new LeapYearCalculator();
+
+            var isLeap = calculator.Isleap(yearNumber);
+
+            isLeap.Should().BeTrue();
+        }
+
         [TestCase(2000)]
         [TestCase(1600)]
         [TestCase(1200)]
@@ -26,7 +38,6 @@ namespace LeapYearKata.Tests
 
             isLeap.Should().BeFalse();
         }
-
         [SetUp]
         public void SetUp()
         {
