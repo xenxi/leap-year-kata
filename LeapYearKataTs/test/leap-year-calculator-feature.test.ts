b3278a0a7ea359d56d93c89ce6c6cd6682c9d620
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-04 11:39:18 +0100

    🎉 init typescript proyect

---
+++ LeapYearKataTs/test/leap-year-calculator-feature.test.ts
@@ -0,0 +1,10 @@
+import { LeapYearCalculator } from "../leap-year-calculator";
+
+//todo: Todos los años divisibles por 400 SON años bisiestos (así, por ejemplo, 2000 fue un año bisiesto),
+//todo: Todos los años divisibles por 100 pero no por 400 NO son años bisiestos (por ejemplo, 1700, 1800 y 1900 NO fueron años bisiestos, NI 2100 será un año bisiesto),
+//todo: Todos los años divisibles por 4 pero no por 100 SON años bisiestos (por ejemplo, 2008, 2012, 2016),
+//todo: Todos los años que no son divisibles por 4 NO son bisiestos (por ejemplo, 2017, 2018, 2019).
+
+describe("LeapYearCalculator should", () => {
+
+});
