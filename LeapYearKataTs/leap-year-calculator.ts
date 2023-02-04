ffcdca606f59e71ad03101c23821e89221c649fb
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-04 12:33:54 +0100

    ♻️ inline class

--- LeapYearKataTs/leap-year-calculator.ts
+++ LeapYearKataTs/leap-year-calculator.ts
@@ -1,18 +1,15 @@
 export class LeapYearCalculator {
+  constructor(private year: number) {}
+
   public static isLeap(yearNumber: number): boolean {
-    const year = new Year(yearNumber);
+    const year = new LeapYearCalculator(yearNumber);
     return year.isLeap();
   }
-}
-
-class Year {
-  constructor(private year: number) {}
 
   public isLeap(): boolean {
     if (this.isDivisibleBy(100) && !this.isDivisibleBy(400)) return false;
     if (!this.isDivisibleBy(4)) return false;
     return true;
   }
-
   private isDivisibleBy = (num: number): boolean => this.year % num == 0;
 }
