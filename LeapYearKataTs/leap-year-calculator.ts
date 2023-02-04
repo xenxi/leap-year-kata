f2f3f86bcfa8c4e49c67b0d9b4b1cad6e5266173
xenxi <antoniom.diaz.moreno@gmail.com>
2023-02-04 12:21:19 +0100

    ♻️ extract method

--- LeapYearKataTs/leap-year-calculator.ts
+++ LeapYearKataTs/leap-year-calculator.ts
@@ -1,7 +1,15 @@
 export class LeapYearCalculator {
   public static isLeap(year: number): boolean {
-    if (year % 100 === 0 && year % 400 !== 0) return false;
-    if (year % 4 !== 0) return false;
+    if (
+      this.isDivisible({ year: year, num: 100 }) &&
+      !this.isDivisible({ year: year, num: 400 })
+    )
+      return false;
+    if (!this.isDivisible({ year: year, num: 4 })) return false;
     return true;
   }
+  private static isDivisible = (params: {
+    year: number;
+    num: number;
+  }): boolean => params.year % params.num == 0;
 }
